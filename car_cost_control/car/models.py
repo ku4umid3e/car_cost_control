fb51a947ddced36af8ee631360f6c1404e76a73c
ku4umid3e <kuchumid3e@gmail.com>
2022-07-15 18:01:00 +0300

    added firs models

--- car_cost_control/car/models.py
+++ car_cost_control/car/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    name = models.CharField(max_length=255)
+    engine_volume = models.DecimalField(max_digits=3, decimal_places=1)
+    fuel_tank_volume = models.IntegerField()
+    mileage = models.IntegerField()
